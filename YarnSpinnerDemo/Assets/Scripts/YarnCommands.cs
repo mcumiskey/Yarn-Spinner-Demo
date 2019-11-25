b7c0774b85bff793d00812770d615d6ce98bab01
Miles <milescumiskey@gmail.com>
2019-11-25 08:58:21 -0500

    initial commit: includes Yarn, YarnSpinner, Merino, and an example project based on the stanley parable

---
+++ YarnSpinnerDemo/Assets/Scripts/YarnCommands.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yarn.Unity.Example {
+
+    public class YarnCommands : MonoBehaviour {
+
+        public GameObject ConfettiParticles;
+        public GameObject SpaceParticles;
+       /// Create a command to use on a sprite
+        [YarnCommand("generateParticles")]
+        public void generateParticles(string particleName) {
+            if(particleName == "space") {
+                Instantiate(SpaceParticles, new Vector3(0, 0, 0), Quaternion.identity);
+            } else {
+                Instantiate(ConfettiParticles, new Vector3(0, 0, 0), Quaternion.identity);
+            }
+        }
+    }
+}
